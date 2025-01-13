cff19e3dcf84fef4422ab48ed08159a438a5a5e3
Cedric Chevalier <cedric.chevalier@cea.fr>
2025-01-13 23:46:58 +0100

    Jacobi (need tests)

--- 02-iterative-relaxation/main.cpp
+++ 02-iterative-relaxation/main.cpp
@@ -1,17 +1,26 @@
-#include "CSRMatrix.h"
-
 #include <iostream>
+
+#include "CSRMatrix.h"
+#include "Jacobi.h"
 
 int main()
 {
     auto A = createLaplacian2D<int, double>(9);
 
-    for (int i = 0; i < A.n; i++)
+    std::vector<double> b(A.n, 1.0);
+    std::vector<double> x(A.n, 0.0);
+
+    int max_iter = 1000;
+    double tolerance = 1e-6;
+
+    auto iter = jacobi(A.n, A, b, x, tolerance, max_iter);
+
+    std::cout << "Number of iterations: " << iter << std::endl;
+
+    std::cout << "Solution:" << std::endl;
+    for (int i = 0; i < A.n; ++i)
     {
-        for (int j = A.row_ptr[i]; j < A.row_ptr[i + 1]; j++)
-        {
-            std::cout << "A[" << i << "," << A.col_idx[j] << "] = " << A.values[j] << std::endl;
-        }
+        std::cout << x[i] << std::endl;
     }
 
     return 0;
