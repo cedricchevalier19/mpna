a35df275e7191533c42f75a6c0534bd3c7a4439a
Cedric Chevalier <cedric.chevalier@cea.fr>
2025-01-13 23:51:09 +0100

    Gauss Seidel, need tests

--- 02-iterative-relaxation/main.cpp
+++ 02-iterative-relaxation/main.cpp
@@ -2,6 +2,7 @@
 
 #include "CSRMatrix.h"
 #include "Jacobi.h"
+#include "GaussSeidel.h"
 
 int main()
 {
@@ -13,14 +14,26 @@ int main()
     int max_iter = 1000;
     double tolerance = 1e-6;
 
-    auto iter = jacobi(A.n, A, b, x, tolerance, max_iter);
-
-    std::cout << "Number of iterations: " << iter << std::endl;
-
-    std::cout << "Solution:" << std::endl;
-    for (int i = 0; i < A.n; ++i)
     {
-        std::cout << x[i] << std::endl;
+        // Solve the system using Jacobi
+        auto iter = jacobi(A.n, A, b, x, tolerance, max_iter);
+        std::cout << "Jacobi, Number of iterations: " << iter << std::endl;
+        // std::cout << "Solution:" << std::endl;
+        // for (int i = 0; i < A.n; ++i)
+        // {
+        //     std::cout << x[i] << std::endl;
+        // }
+    }
+
+    {
+        // Solve the system using Gauss-Seidel
+        auto iter = gaussSeidel(A.n, A, b, x, tolerance, max_iter);
+        std::cout << "Gauss-Seidel, Number of iterations: " << iter << std::endl;
+        // std::cout << "Solution:" << std::endl;
+        // for (int i = 0; i < A.n; ++i)
+        // {
+        //     std::cout << x[i] << std::endl;
+        // }
     }
 
     return 0;
